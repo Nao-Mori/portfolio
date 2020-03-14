ce53072b94760d628d53a8f24182f2845ae461e9
Nao Mori <lockn980@gmail.com>
2020-03-14 09:28:31 +0100

    Adding "Blog" section
    linking to https://blog.nao-mori.com

--- App.js
+++ App.js
@@ -3,24 +3,29 @@ import { animated, useSpring } from "react-spring"
 import Projects from "./Components/Projects"
 import AboutMe from "./Components/AboutMe"
 import Contact from "./Components/Contact"
+import Blog from "./Components/Blog"
 import Background from "./Background"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 import { Link, Element } from 'react-scroll'
 
-const App=props=>{
+const scrollDuration = 400
+
+const App = props => {
     const [navshow,setNavshow]=React.useState(false)
     const [trick,setTrick]=React.useState(false)
     const [pos,setPos]=React.useState(0)
     const [bgcolor,setBgcolor]=React.useState([])
     const [current,setCurrent]=React.useState([])
+    const [laterLoad,setLaterLoad]=React.useState(false)
     
     const projectRef = React.useRef()
     const aboutmeRef = React.useRef()
+    const blogRef = React.useRef()
     const contactRef = React.useRef()
     const top = React.useRef()
-    
+
 
     const fade = useSpring({
         from: {opacity:0},
@@ -37,14 +42,29 @@ const App=props=>{
     React.useEffect(()=>{
         setInterval(()=>{
             if(aboutmeRef.current.offsetTop<=window.scrollY){
+                if(!laterLoad) setLaterLoad(true)
                 setNavshow(true)
-                if(contactRef.current.offsetTop<=window.scrollY) setCurrent(3)
-                else if(projectRef.current.offsetTop<=window.scrollY) setCurrent(2)
-                else setCurrent(1)
+                if(contactRef.current.offsetTop<=window.scrollY) {
+                    setCurrent(4)
+                    document.title="Nao Mori | Contact"
+                }
+                else if(blogRef.current.offsetTop<=window.scrollY) {
+                    setCurrent(3)
+                    document.title="Nao Mori | Blog"
+                }
+                else if(projectRef.current.offsetTop<=window.scrollY) {
+                    setCurrent(2)
+                    document.title="Nao Mori | Projects"
+                }
+                else {
+                    setCurrent(1)
+                    document.title="Nao Mori | About Me"
+                }
             }
             else {
                 setNavshow(false)
                 setCurrent(0)
+                document.title="Nao Mori"
             }
             if(contactRef.current.offsetTop<=window.scrollY) setTrick(true)
             else setTrick(false)
@@ -62,36 +82,45 @@ const App=props=>{
             <Background pos={pos} bgcolor={bgcolor} trick={trick}/>
             <animated.div className="navback" style={navFade}>
                 <div style={{display: "flex"}}>
-                    <Link activeClass="active" to="top" spy={false} smooth={true} offset={0} duration={300}>
+                    <Link activeClass="active" to="top" spy={false} smooth={true} offset={0} duration={scrollDuration}>
                         <img alt="icon" src="/android-icon-36x36.png" style={{width:"30px",height:"30px",marginLeft:"15px"}} className="pointer"/>
                     </Link>
-                    <Link activeClass="active" to="aboutme" spy={false} smooth={true} offset={0} duration={300}>
+                    <Link activeClass="active" to="aboutme" spy={false} smooth={true} offset={0} duration={scrollDuration}>
                         <h5 className={current===1?"navlink-active navlink":"navlink"}>About Me</h5>
                     </Link>
-                    <Link activeClass="active" to="project" spy={false} smooth={true} offset={0} duration={300}>
+                    <Link activeClass="active" to="project" spy={false} smooth={true} offset={0} duration={scrollDuration}>
                         <h5 className={current===2?"navlink-active navlink":"navlink"}>Projects</h5>
                     </Link>
-                    <Link activeClass="active" to="contact" spy={false} smooth={true} offset={0} duration={300}>
-                        <h5 className={current===3?"navlink-active navlink":"navlink"}>Contact</h5>
+                    <Link activeClass="active" to="blog" spy={false} smooth={true} offset={0} duration={scrollDuration}>
+                        <h5 className={current===3?"navlink-active navlink":"navlink"}>Blog</h5>
+                    </Link>
+                    <Link activeClass="active" to="contact" spy={false} smooth={true} offset={0} duration={scrollDuration}>
+                        <h5 className={current===4?"navlink-active navlink":"navlink"}>Contact</h5>
                     </Link>
                 </div>
             </animated.div>
             <div style={{paddingTop:"15vw",height:"100vh", textAlign:"center"}}>
                 <animated.h1 style={fade}>Hey, I'm Nao Mori.</animated.h1>
-                <Link activeClass="active" to="aboutme" spy={false} smooth={true} offset={0} duration={300}>
+                <Link activeClass="active" to="aboutme" spy={false} smooth={true} offset={0} duration={scrollDuration}>
                     <button>Check Me Out</button>
                 </Link>
             </div>
             <div ref={aboutmeRef} name="aboutme"><AboutMe /></div>
-            <div ref={projectRef} name="project"><Projects /></div>
+            {laterLoad?
+                <div ref={projectRef} name="project"><Projects /></div>
+            :null}
+            <div ref={blogRef} name="blog"><Blog /></div>
             <div ref={contactRef} name="contact"><Contact goTop={()=>scrollTo(top)} /></div>
             <div className="spacer-bottom"/>
             <footer className="text-center">
-                <a href="https://github.com/nao-mori" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon className="mr-2 brand-icon" size="3x" icon={faGithub}/>
+                <a href="https://github.com/nao-mori/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="mr-4 brand-icon" size="3x" icon={faGithub}/>
                 </a>
-                <a href="https://www.linkedin.com/in/%E9%A0%86-%E6%A3%AE-878223171/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon className="ml-2 brand-icon" size="3x" icon={faLinkedin}/>
+                <a href="https://www.facebook.com/profile.php?id=100012798553121" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="brand-icon" size="3x" icon={faFacebook}/>
+                </a>
+                <a href="https://www.linkedin.com/in/nao-mori-878223171/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="ml-4 brand-icon" size="3x" icon={faLinkedin}/>
                 </a>
                 <div className="pt-2">©Nao Mori 2020</div>
             </footer>
--- Background.js
+++ Background.js
@@ -6,17 +6,26 @@ for (var i = 1; i < 31; i++) {
     arr.push({name:`/tulip/tulip animation-${i}.png`,opa:0});
 }
 
-const Bg=({pos,bgcolor,trick})=>{
+const Bg=({pos,bgcolor})=>{
     const [bgs]=React.useState(arr)
 
     const fade = useSpring({
         from: {opacity:0},
-        opacity: 1
+        opacity: 1,
+        zIndex: -1
      })
+
+     const bgStyle={
+        position: "fixed",
+        zIndex: "-2",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor:`rgb(${bgcolor})`   
+    }
 
     return(
         <div>
-            <div className="bg" style={{backgroundColor:`rgb(${bgcolor})`}}/>
+            <div style={bgStyle}/>
             {bgs.map((anim,key)=>{
                 return(
                     <animated.div key={key} className="icon" style={fade}>
